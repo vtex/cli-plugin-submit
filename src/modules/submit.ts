8d2d017ce3c031af7db46765f97d374b82558777
Luciano Júnior <luciannojunior@gmail.com>
2020-07-23 17:26:34 -0300

    Fixing prettier issues

--- src/modules/submit.ts
+++ src/modules/submit.ts
@@ -4,7 +4,7 @@ export const submitApp = async (appId?: string) => {
   const appToSubmit = appId || (await ManifestEditor.getManifestEditor()).appLocator
 
   // validate if there's an appId
-    // ask for the github username
+  // ask for the github username
   // ask for the live URL
   // create the client and call the method
   // show the pullrequest URL
