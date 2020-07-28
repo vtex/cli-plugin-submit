f68665295056882f805c161b73076912bf84a249
Elizabeth Villamizar <elizabeth@vtex.com.br>
2020-07-28 08:56:05 -0500

    fixing issues

--- src/modules/submit.ts
+++ src/modules/submit.ts
@@ -4,49 +4,48 @@ import { ManifestEditor } from 'vtex/build/api/manifest'
 
 import AppsReview from '../clients/appsReview'
 
-export const submitApp = async (appId?: string) => {
-  const appToSubmit =
-    appId ?? (await ManifestEditor.getManifestEditor()).appLocator
+export const submitApp = async (appToSubmit?: string) => {
+  const appId =
+    appToSubmit ?? (await ManifestEditor.getManifestEditor()).appLocator
 
-  // validate if there's an appId
+  logger.info(
+    "We will open a Pull Request with your app's code. You'll be able to check the review status directly from Gitub."
+  )
 
-  // ask for the github username
   const { githubUsername } = await inquirer.prompt([
     {
       name: 'githubUsername',
-      message: 'Submit your github username',
+      message: 'Enter your Github username',
       type: 'input',
     },
   ])
 
-  // ask for the live URL
   const { liveUrl } = await inquirer.prompt([
     {
       name: 'liveUrl',
-      message: 'Submit a live Url where we can test your app',
+      message:
+        'Enter a URL from where we can test your app working. It can be in your workspace',
       type: 'input',
     },
   ])
 
   logger.info('We are validating your data, please wait a few seconds')
-  // create the client and call the method
+
   const client = AppsReview.createClient()
 
   try {
     const pullRequestUrl = await client.submitApp({
-      appId: appToSubmit,
+      appId,
       githubUsername,
       liveUrl,
     })
 
-    // show the pullrequest URL
     logger.info(`We've submitted the app ${appToSubmit} to review!`)
     logger.info(
       "You'll receive an e-mail inviting you to the newly-created repository where you'll be able to follow the status"
     )
     logger.info(`The pull request for this version is at: ${pullRequestUrl}`)
   } catch (e) {
-    // show error if something went wrong
     logger.error(e.response.data.message)
   }
 }
